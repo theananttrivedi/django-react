5a3b8d060f5aeacb0b802531ffb46fd561c444a4
Anant Trivedi <theananttrivedi@gmail.com>
2020-07-21 12:37:28 +0530

    First Commit

---
+++ ui/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+//import logo from './logo.svg';
+//import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
